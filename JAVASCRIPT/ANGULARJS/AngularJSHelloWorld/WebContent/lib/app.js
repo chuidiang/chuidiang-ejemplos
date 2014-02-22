15195490385b5175b49d2237a7c57062a07a2adf
chuidiang <chuidiang@gmail.com>
2014-02-22 13:30:14 +0100

    AngularJS Basic Examples

---
+++ JAVASCRIPT/ANGULARJS/AngularJSHelloWorld/WebContent/lib/app.js
@@ -0,0 +1,24 @@
+function HelloController($scope) {
+	$scope.greeting = 'hello';
+}
+
+function CheckController ($scope) {
+	$scope.checkbox = 'true';
+	$scope.visible = true;
+	
+	$scope.toggle = function () {
+		$scope.visible = !$scope.visible;
+	};
+}
+
+function RepeatController ($scope) {
+	$scope.counter=1;
+	$scope.array=[0];
+	
+	$scope.updateModel = function () {
+		$scope.array=[];
+		for (var i=0;i<$scope.counter;i++){
+			$scope.array.push(i);
+		}
+	};
+}
