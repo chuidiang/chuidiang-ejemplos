ae9cefdbd36b852186d9df5971f864e74b90f379
chuidiang <chuidiang@gmail.com>
2014-02-25 19:42:29 +0100

    Communicating AngularJS with Server

---
+++ JAVASCRIPT/ANGULARJS/AngularJSService/WebContent/lib/app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var myApp = angular.module('myApp',[]);
+
+myApp.service('Users',function($http){
+	this.query = function() {
+	   return $http({method:'GET',url:'users.json'});
+	};
+});
+
+myApp.controller('HttpController',function($scope,$http){
+	$http.get('users.json').success(function(data){
+		$scope.sameItems=data;
+	});
+});
+
+myApp.controller('ServiceController',function($scope,$http,Users){
+	Users.query().success(function (data) {
+		$scope.items=data;
+	});
+});
