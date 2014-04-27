4a34d18a8c9aa7cce9a43a00bf9f9550805999c4
chuidiang <chuidiang@gmail.com>
2014-04-27 18:30:35 +0200

    Con GridBagLayout.

--- JAVA_SE/src/com/chuidiang/ejemplos/BotonInferiorDerecha.java
+++ JAVA_SE/src/com/chuidiang/ejemplos/BotonInferiorDerecha.java
@@ -2,9 +2,12 @@ package com.chuidiang.ejemplos;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 /**
@@ -15,6 +18,31 @@ import javax.swing.JPanel;
 public class BotonInferiorDerecha {
 
    public static void main(String[] args) {
+      panelConFlowLayout();
+      conGridBagLayout();
+   }
+
+   private static void conGridBagLayout() {
+      JFrame v = new JFrame();
+      v.getContentPane().setLayout(new GridBagLayout());
+      JLabel dummy = new JLabel();
+      GridBagConstraints gc = new GridBagConstraints();
+      gc.gridx=0;
+      gc.gridy=0;
+      gc.weightx=1.0;
+      gc.weighty=1.0;
+      v.getContentPane().add(dummy,gc);
+      gc.gridx=1;
+      gc.gridy=1;
+      gc.weightx=0.0;
+      gc.weighty=0.0;
+      v.getContentPane().add(new JButton("el boton"),gc);
+      v.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      v.pack();
+      v.setVisible(true);
+   }
+
+   private static void panelConFlowLayout() {
       JFrame v = new JFrame();
       JPanel panel2 = new JPanel(new FlowLayout(FlowLayout.TRAILING));
       panel2.add(new JButton("el boton"));
