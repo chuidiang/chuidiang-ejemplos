21db4e31cbf66dbcf4886d5d02ef315e5a88fb77
chuidiang <chuidiang@gmail.com>
2015-12-19 12:06:46 +0100

    Comparando campos de formulario de JSF con PrimeFaces.

--- JSF/primefaces-example/src/main/java/com/chuidiang/examples/primefaces/HelloWorld.java
+++ JSF/primefaces-example/src/main/java/com/chuidiang/examples/primefaces/HelloWorld.java
@@ -1,13 +1,24 @@
 package com.chuidiang.examples.primefaces;
 
+import java.util.logging.Logger;
+
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 
 @ManagedBean(name = "helloWorld", eager = true)
+@SessionScoped
 public class HelloWorld {
+   private static final Logger LOG = Logger.getLogger(HelloWorld.class.getName());
+   private String message="Hello World!";
+   
    public HelloWorld() {
-      System.out.println("HelloWorld started!");
+      LOG.info("ManagedBean HelloWorld started");
    }
    public String getMessage() {
-      return "Hello World!";
+      return this.message;
+   }
+   public void setMessage(String message){
+      LOG.info("Received "+message);
+      this.message=message;
    }
 }
