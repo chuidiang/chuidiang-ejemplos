0dae50641dc399d0a6196de8d6db60f5efd2b14c
chuidiang <chuidiang@gmail.com>
2016-01-21 22:26:43 +0100

    Arreglado test. Funciona finalmente testeando con OpenEJB

--- EJB/EJB_Example/src/test/java/com/chuidiang/examples/EJB_Example/TestNewSessionBean.java
+++ EJB/EJB_Example/src/test/java/com/chuidiang/examples/EJB_Example/TestNewSessionBean.java
@@ -1,53 +1,43 @@
 package com.chuidiang.examples.EJB_Example;
 
 import javax.ejb.EJB;
-import javax.inject.Inject;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.arquillian.junit.InSequence;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import com.chuidiang.examples.ejb.StatelesBean;
 
-@Ignore
 @RunWith(Arquillian.class)
 public class TestNewSessionBean {
-
-   @Deployment
-   public static JavaArchive createDeployment() {
-       return ShrinkWrap.create(JavaArchive.class,"test.jar")
-           .addClasses(StatelessBeanImpl.class,StatefullBean.class)
-           .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
-   }
    
-   @Inject
+   @Deployment(name="dep2", order=2)
+   public static JavaArchive createDeployment2() {
+       return ShrinkWrap.create(JavaArchive.class,"test2.jar")
+           .addClasses(StatelessBeanImpl.class,StatefullBean.class,StatelesBean.class)
+//           .setManifest(new StringAsset("Dependencies:com.chuidiang.examples.ejb_interfaces"))
+           .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+       
+   }
+
+   @EJB
    private StatelesBean statelessBeanImpl;
 
    @EJB
    private StatefullBean statefulBean;
    
-   @Test
-   @InSequence(1)
+   @Test 
    public void testSayHello() {
       Assert.assertNotNull(statelessBeanImpl);
-      Assert.assertEquals("Hello", statelessBeanImpl.sayHello());
+      Assert.assertEquals(" Hello from an EJB !! ", statelessBeanImpl.sayHello());
       Assert.assertNotNull(statefulBean);
       statefulBean.setValue(11);
       Assert.assertEquals(11, statefulBean.getValue());
    }
 
-//   @Test
-//   @InSequence(2)
-//   public void testSayHello2() {
-//      Assert.assertNotNull(statelessBeanImpl);
-//      Assert.assertEquals("Hello", statelessBeanImpl.sayHello());
-//      Assert.assertEquals(11, statefulBean.getValue());
-//   }
 }
