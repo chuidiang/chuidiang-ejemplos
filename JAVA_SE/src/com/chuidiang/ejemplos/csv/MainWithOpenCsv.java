d83e163a25cc2edbcc5209213f5bf6ddce59a63f
chuidiang <chuidiang@gmail.com>
2016-03-07 20:33:23 +0100

    Ejemplo lectura CSV con java

---
+++ JAVA_SE/src/com/chuidiang/ejemplos/csv/MainWithOpenCsv.java
@@ -0,0 +1,38 @@
+package com.chuidiang.ejemplos.csv;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
+import com.opencsv.CSVReader;
+
+public class MainWithOpenCsv {
+
+   public static final char SEPARATOR=';';
+   public static final char QUOTE='"';
+
+   public static void main(String[] args) {
+
+      CSVReader reader = null;
+      try {
+         reader = new CSVReader(new FileReader("resources/Libro2.csv"),SEPARATOR,QUOTE);
+         String[] nextLine=null;
+         
+         while ((nextLine = reader.readNext()) != null) {
+            System.out.println(Arrays.toString(nextLine));
+         }
+         
+      } catch (Exception e) {
+         System.err.println("Error!! " + e.getMessage());
+      } finally {
+         if (null != reader) {
+            try {
+               reader.close();
+            } catch (IOException e) {
+               System.err.println("Error closing file!! " + e.getMessage());
+            }
+         }
+      }
+   }
+
+}
---
+++ JAVA_SE/src/com/chuidiang/ejemplos/csv/MainWithoutLibrary.java
@@ -0,0 +1,58 @@
+package com.chuidiang.ejemplos.csv;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
+/**
+ * Example to parse simple CSV files
+ * @author Chuidiang
+ *
+ */
+public class MainWithoutLibrary {
+   
+   public static final String SEPARATOR=";";
+   public static final String QUOTE="\"";
+   
+   public static void main(String[] args) {
+      
+      BufferedReader br = null;
+      
+      try {
+         
+         br =new BufferedReader(new FileReader("resources/Libro1.csv"));
+         String line = br.readLine();
+         while (null!=line) {
+            String [] fields = line.split(SEPARATOR);
+            System.out.println(Arrays.toString(fields));
+            
+            fields = removeTrailingQuotes(fields);
+            System.out.println(Arrays.toString(fields));
+            
+            line = br.readLine();
+         }
+         
+      } catch (Exception e) {
+         System.err.println("Error! "+e.getMessage());
+      } finally {
+         if (null!=br){
+            try {
+               br.close();
+            } catch (IOException e) {
+               System.err.println("Error closing file !! "+e.getMessage());
+            }
+         }
+      }
+      
+   }
+   
+   private static String[] removeTrailingQuotes(String[] fields) {
+      String result[] = new String[fields.length];
+      for (int i=0;i<result.length;i++){
+         result[i] = fields[i].replaceAll("^"+QUOTE, "").replaceAll(QUOTE+"$", "");
+      }
+      return result;
+   }
+
+}
