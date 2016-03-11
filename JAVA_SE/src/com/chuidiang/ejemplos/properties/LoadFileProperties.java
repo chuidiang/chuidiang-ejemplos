34ce09b8736f853c4e8f21d443c4e1378c7bb66f
chuidiang <chuidiang@gmail.com>
2016-03-11 21:53:26 +0100

    Añadido como recorrer la clase Properties para extraer todas sus propiedades

--- JAVA_SE/src/com/chuidiang/ejemplos/properties/LoadFileProperties.java
+++ JAVA_SE/src/com/chuidiang/ejemplos/properties/LoadFileProperties.java
@@ -2,6 +2,7 @@ package com.chuidiang.ejemplos.properties;
 
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.Enumeration;
 import java.util.Properties;
 
 
@@ -13,6 +14,13 @@ public class LoadFileProperties {
       try {
          p.load(new FileReader("files/properties/config.properties"));
          System.out.println("uno="+p.getProperty("uno"));
+         System.out.println("cuatro="+p.getProperty("cuatro", "default value"));
+         
+         Enumeration<Object> keys = p.keys();
+         while (keys.hasMoreElements()){
+            Object key = keys.nextElement();
+            System.out.println(key + " = "+ p.get(key));
+         }
          
          p.setProperty("cuatro", "4");
          p.store(new FileWriter("target/out.properties"),"Some comment");
