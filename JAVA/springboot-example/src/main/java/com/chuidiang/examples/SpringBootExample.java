6ab8942854871f167fcd89fbb781baae561ce5e1
chuidiang <chuidiang@gmail.com>
2016-03-15 22:04:46 +0100

    El Hola Mundo de la web de Spring Boot copiado.

---
+++ JAVA/springboot-example/src/main/java/com/chuidiang/examples/SpringBootExample.java
@@ -0,0 +1,20 @@
+package com.chuidiang.examples;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@EnableAutoConfiguration
+public class SpringBootExample {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SpringBootExample.class, args);
+    }
+}
