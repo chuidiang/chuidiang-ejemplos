4b6065120a2793be232b1ae6f8279f2d8db37075
chuidiang <chuidiang@gmail.com>
2016-03-15 22:21:13 +0100

    Ahora le ha tocado el turno al scheduled de spring boot

---
+++ JAVA/springboot-example/src/main/java/com/chuidiang/examples/scheduled_task/ScheduledTask.java
@@ -0,0 +1,18 @@
+package com.chuidiang.examples.scheduled_task;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduledTask {
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+    @Scheduled(fixedRate = 5000)
+    public void reportCurrentTime() {
+        System.out.println("The time is now " + dateFormat.format(new Date()));
+    }
+}
