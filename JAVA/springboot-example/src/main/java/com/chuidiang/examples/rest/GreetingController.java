55b18e56c7fd1dba5d015a71ddd9c0361e9cbf64
chuidiang <chuidiang@gmail.com>
2016-03-18 18:06:35 +0100

    restful springboot. Eliminados warning de una clase.

--- JAVA/springboot-example/src/main/java/com/chuidiang/examples/rest/GreetingController.java
+++ JAVA/springboot-example/src/main/java/com/chuidiang/examples/rest/GreetingController.java
@@ -1,7 +1,6 @@
 package com.chuidiang.examples.rest;
 
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +10,7 @@ import org.springframework.web.servlet.mvc.multiaction.NoSuchRequestHandlingMeth
 
 @RestController
 public class GreetingController {
-
-   private static final String template = "Hello, %s!";
-   private final AtomicLong counter = new AtomicLong();
+   
    private Data data = new Data();
 
    @RequestMapping(method=RequestMethod.GET, path="/greeting")
