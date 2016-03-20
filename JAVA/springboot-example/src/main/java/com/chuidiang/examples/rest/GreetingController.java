da0de9b258c67656b79714a0b188664a80448bda
chuidiang <chuidiang@gmail.com>
2016-03-20 09:45:56 +0100

    spring boot restful. Eliminados import sobrantes

--- JAVA/springboot-example/src/main/java/com/chuidiang/examples/rest/GreetingController.java
+++ JAVA/springboot-example/src/main/java/com/chuidiang/examples/rest/GreetingController.java
@@ -2,16 +2,12 @@ package com.chuidiang.examples.rest;
 
 import java.util.List;
 
-import javax.sound.sampled.DataLine;
-import javax.websocket.server.PathParam;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.multiaction.NoSuchRequestHandlingMethodException;
 
