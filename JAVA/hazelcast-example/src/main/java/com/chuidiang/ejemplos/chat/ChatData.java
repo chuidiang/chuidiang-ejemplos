4d14a6aa38e53858ef518fbddaade03965f18abc
chuidiang <chuidiang@gmail.com>
2016-05-24 22:48:37 +0200

    Serial uuid para clase Serializable

--- JAVA/hazelcast-example/src/main/java/com/chuidiang/ejemplos/chat/ChatData.java
+++ JAVA/hazelcast-example/src/main/java/com/chuidiang/ejemplos/chat/ChatData.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.Date;
 
 public class ChatData implements Serializable {
+   private static final long serialVersionUID = 2015942905269624152L;
    @Override
    public String toString() {
       return "" + date + " " + user + ": " + text;
