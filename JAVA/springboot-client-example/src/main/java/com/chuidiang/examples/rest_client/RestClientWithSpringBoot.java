11e18f817f599a1abdcd79e2c564f66c0e183039
chuidiang <chuidiang@gmail.com>
2016-09-17 13:23:22 +0200

    Refactor: Repeated code extracted to common class.

--- JAVA/springboot-client-example/src/main/java/com/chuidiang/examples/rest_client/RestClientWithSpringBoot.java
+++ JAVA/springboot-client-example/src/main/java/com/chuidiang/examples/rest_client/RestClientWithSpringBoot.java
@@ -2,11 +2,17 @@ package com.chuidiang.examples.rest_client;
 
 import java.util.logging.Logger;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.web.client.RestTemplate;
 
-public class RestClientWhitoutSpringBoot  {
-
+public class RestClientWithSpringBoot implements CommandLineRunner {
    public static void main(String args[]) {
+      SpringApplication.run(RestClientWithSpringBoot.class);
+   }
+
+   @Override
+   public void run(String... args) throws Exception {
       RestTemplate restTemplate = new RestTemplate();
       RestRequests restRequests = new RestRequests();
 
@@ -17,10 +23,10 @@ public class RestClientWhitoutSpringBoot  {
 
       restRequests.postGreeting(restTemplate);
       restRequests.getAndShowGreetings(restTemplate);
-      
+
       restRequests.deleteGreeting(restTemplate);
       restRequests.getAndShowGreetings(restTemplate);
-      
+
       restRequests.getOneGreeting(restTemplate);
    }
 
---
+++ JAVA/springboot-client-example/src/main/java/com/chuidiang/examples/rest_client/RestRequests.java
@@ -0,0 +1,52 @@
+package com.chuidiang.examples.rest_client;
+
+import java.util.logging.Logger;
+
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+public class RestRequests {
+   private static final Logger log = Logger
+         .getLogger(RestRequests.class.toString());
+
+   public void getOneGreeting(RestTemplate restTemplate) {
+      // We want Greeting at index=0
+      Greeting aGreeting = restTemplate
+            .getForObject("http://localhost:8080/greeting/0", Greeting.class);
+      System.out.println("Get=0 -> " + aGreeting);
+
+   }
+
+   public void deleteGreeting(RestTemplate restTemplate) {
+      // We want to delete Greeting at index=2
+      restTemplate.delete("http://localhost:8080/greeting/2");
+
+   }
+
+   public void putGreeting(RestTemplate restTemplate) {
+      Greeting aGreeting = new Greeting(1, "Modified Greeting");
+
+      // We want to modify the Greeting at index=1
+      restTemplate.put("http://localhost:8080/greeting/1", aGreeting);
+   }
+
+   public void postGreeting(RestTemplate restTemplate) {
+      String name = "New Greeting";
+      MultiValueMap<String, Object> values = new LinkedMultiValueMap<String, Object>();
+      values.add("content", name);
+      Greeting gretting = restTemplate.postForObject(
+            "http://localhost:8080/greeting", values, Greeting.class);
+      System.out.println("POSTED: " + gretting);
+   }
+
+   public void getAndShowGreetings(RestTemplate restTemplate) {
+      Greeting[] greetings = restTemplate
+            .getForObject("http://localhost:8080/greeting", Greeting[].class);
+
+      for (Greeting greeting : greetings) {
+         System.out.println(greeting);
+      }
+      System.out.println("----------------");
+   }
+}
