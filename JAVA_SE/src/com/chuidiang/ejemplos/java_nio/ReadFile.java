907cc7ffc22fdb33798763bf965d05532d55515f
chuidiang <chuidiang@gmail.com>
2016-09-23 18:00:53 +0200

    Read Lines from a File with java 8 Stream.

--- JAVA_SE/src/com/chuidiang/ejemplos/java_nio/ReadFile.java
+++ JAVA_SE/src/com/chuidiang/ejemplos/java_nio/ReadFile.java
@@ -3,13 +3,28 @@ package com.chuidiang.ejemplos.java_nio;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.stream.Stream;
 
 public class ReadFile {
 
+   public static String fileName = "src/com/chuidiang/ejemplos/java_nio/ReadFile.java";
+
    public static void main(String[] args) throws IOException {
+      readAsBytes();
+      readAsLines();
+   }
+
+   private static void readAsLines() throws IOException {
+      //read file into stream, try-with-resources
+      Stream<String> stream = Files.lines(Paths.get(fileName)); 
+      stream.forEach(System.out::println);
+   }
+
+   private static void readAsBytes() throws IOException {
       Path path = Paths
             .get("src/com/chuidiang/ejemplos/java_nio/ReadFile.java");
       FileChannel channel = FileChannel.open(path, StandardOpenOption.READ);
@@ -20,7 +35,7 @@ public class ReadFile {
          while (bytesRead != -1) {
             buffer.flip();
             while (buffer.hasRemaining()) {
-               System.out.print((char) buffer.get());
+               System.out.print((char)buffer.get());
             }
             buffer.compact();
             bytesRead = channel.read(buffer);
