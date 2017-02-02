c2b3b165589925f1c6d5269e51feb690924ee7cf
chuidiang <chuidiang@gmail.com>
2017-02-02 23:37:33 +0100

    Jugando con OSGI e IntelliJ

---
+++ OSGI/OSGIMavenMultiProject/OSGIModule2/src/main/java/com/chuidiang/examples/osgi_module2/Activator.java
@@ -0,0 +1,22 @@
+package com.chuidiang.examples.osgi_module2;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by JAVIER on 01/02/2017.
+ */
+public class Activator implements BundleActivator{
+    private static final Logger log = LoggerFactory.getLogger(Activator.class);
+    @Override
+    public void start(BundleContext bundleContext) throws Exception {
+        log.info("Module 2 Start");
+    }
+
+    @Override
+    public void stop(BundleContext bundleContext) throws Exception {
+
+    }
+}
