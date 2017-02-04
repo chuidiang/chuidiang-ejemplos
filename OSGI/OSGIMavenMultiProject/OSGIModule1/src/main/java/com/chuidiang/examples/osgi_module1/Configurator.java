7e04a800d1e5579ecbd1312a4d38b90d8d10aea7
chuidiang <chuidiang@gmail.com>
2017-02-04 17:51:26 +0100

    Jugando con felix configadmin y fileinstall

---
+++ OSGI/OSGIMavenMultiProject/OSGIModule1/src/main/java/com/chuidiang/examples/osgi_module1/Configurator.java
@@ -0,0 +1,42 @@
+package com.chuidiang.examples.osgi_module1;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.service.cm.ConfigurationException;
+import org.osgi.service.cm.ManagedService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.*;
+
+/**
+ * Created by JAVIER on 04/02/2017.
+ */
+public class Configurator implements ManagedService {
+    private static Logger log = LoggerFactory.getLogger(Configurator.class);
+
+    @Override
+    public void updated(Dictionary<String, ?> dictionary) throws ConfigurationException {
+        if (null==dictionary){
+            log.warn("Dictionary is null");
+            return;
+        }
+
+        Enumeration<String> keys = dictionary.keys();
+        while (keys.hasMoreElements()){
+            String key=keys.nextElement();
+            log.info("update: "+key + " = "+dictionary.get(key));
+        }
+    }
+
+    public void start(Map<String,?> properties){
+        log.info("Configurator starts ");
+        for (Object key:properties.keySet()){
+            log.info("start: key="+key);
+            log.info("start: value="+properties.get(key));
+        }
+    }
+
+    public void stop(){
+        log.info("Configurator stop");
+    }
+}
