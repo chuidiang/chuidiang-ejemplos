1471f20f94557b2de0e18b9f4113799af1fd1541
chuidiang <chuidiang@gmail.com>
2017-05-19 17:05:34 +0200

    Jugando con ProxyMetaClass en Groovy

--- GROOVY/groovy-examples/src/main/groovy/com/chuidiang/examples/ExpandoExamples.groovy
+++ GROOVY/groovy-examples/src/main/groovy/com/chuidiang/examples/ExpandoExamples.groovy
@@ -21,11 +21,54 @@ class ExpandoExamples {
         MyClass myInstance = new MyClass();
         myInstance.someMethod("Some parameter", "Another Parameter")
 
+        // A proxy for capturing all method calls of a class
+        def proxy = ProxyMetaClass.getInstance(InterceptedCalculator)
+        proxy.interceptor = new CalculatorInterceptor()
+
+        proxy.use {
+            InterceptedCalculator calculator = new InterceptedCalculator()
+            println(calculator.add(1,2))
+        }
+
+        // proxy all Calculators.
+        InterceptedCalculator.metaClass = proxy
+        InterceptedCalculator calculator2 = new InterceptedCalculator()
+        println(calculator2.add(-4,-5))
     }
 }
 
 class MyClass {
     def methodMissing(String name, args){
         println "${name}(${args}) doesn't exist  :("
+    }
+}
+
+class InterceptedCalculator {
+    public int add(int a, int b){
+        a+b
+    }
+}
+
+class CalculatorInterceptor implements Interceptor {
+
+    @Override
+    Object beforeInvoke(Object object, String methodName, Object[] arguments) {
+        println("beforeInvoke : ${methodName}(${arguments})")
+        if (arguments.size()>0){
+            arguments[0]=33
+        }
+        return true
+    }
+
+    @Override
+    Object afterInvoke(Object object, String methodName, Object[] arguments, Object result) {
+        println("afterInvoke : ${methodName}(${arguments})")
+        result
+    }
+
+    @Override
+    boolean doInvoke() {
+        println("doInvoke")
+        true
     }
 }
