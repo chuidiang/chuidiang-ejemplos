ea0672f5bd8cb48e8cb2cfe44e43188e1d5e81b2
chuidiang <chuidiang@gmail.com>
2017-11-25 13:34:01 +0100

    Proyecto ejemplo boot, rest, web, angular y eventbus todo junto

---
+++ JAVA/boot-eventbus-rest-angular/src/main/groovy/com/chuidiang/booteventbusrestangular/BootEventbusRestAngularApplication.groovy
@@ -0,0 +1,12 @@
+package com.chuidiang.booteventbusrestangular
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class BootEventbusRestAngularApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run BootEventbusRestAngularApplication, args
+	}
+}
