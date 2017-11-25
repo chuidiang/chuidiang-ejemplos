f0714bfb1582d345575f1c3712588367502dba84
chuidiang <chuidiang@gmail.com>
2017-11-25 14:24:52 +0100

    Un mini rest con boot

---
+++ JAVA/boot-eventbus-rest-angular/src/main/groovy/com/chuidiang/booteventbusrestangular/rest/RandomNumber.groovy
@@ -0,0 +1,8 @@
+package com.chuidiang.booteventbusrestangular.rest
+
+/**
+ * Created by chuidiang on 25/11/17.
+ */
+class RandomNumber {
+    double value
+}
---
+++ JAVA/boot-eventbus-rest-angular/src/main/groovy/com/chuidiang/booteventbusrestangular/rest/RandomResource.groovy
@@ -0,0 +1,18 @@
+package com.chuidiang.booteventbusrestangular.rest
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * Created by chuidiang on 25/11/17.
+ */
+@RestController
+@RequestMapping("/random")
+class RandomResource {
+    @RequestMapping(method=RequestMethod.GET)
+    RandomNumber get(){
+        new RandomNumber (value:Math.random())
+    }
+}
