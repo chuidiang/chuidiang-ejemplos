95bb3f30d3fa530f6eeffaeea00c73faf6a78b60
chuidiang <chuidiang@gmail.com>
2018-08-04 17:35:34 +0200

    usando netty handlers para lineas

---
+++ JAVA/netty-example/src/main/java/com/chuidiang/examples4/ServerWithNettyHandlers.java
@@ -0,0 +1,67 @@
+package com.chuidiang.examples4;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.string.LineEncoder;
+import io.netty.handler.codec.string.StringDecoder;
+
+import java.nio.charset.Charset;
+
+public class ServerWithNettyHandlers {
+    private int port;
+    private ServerHandler serverHandler=new ServerHandler();
+
+    public ServerWithNettyHandlers(int port) {
+        this.port = port;
+    }
+
+    public void run() throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup(); // (1)
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap(); // (2)
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class) // (3)
+                    .childHandler(new ChannelInitializer<SocketChannel>() { // (4)
+                        @Override
+                        public void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new LineBasedFrameDecoder(1000));
+                            ch.pipeline().addLast(new LineEncoder(Charset.defaultCharset()));
+                            ch.pipeline().addLast(new StringDecoder(Charset.defaultCharset()));
+                            ch.pipeline().addLast(serverHandler);
+
+                        }
+                    })
+                    .option(ChannelOption.SO_BACKLOG, 128)          // (5)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true); // (6)
+
+            // Bind and start to accept incoming connections.
+            ChannelFuture f = b.bind(port).sync(); // (7)
+
+            // Wait until the server socket is closed.
+            // In this example, this does not happen, but you can do that to gracefully
+            // shut down your server.
+            f.channel().closeFuture().sync();
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        int port;
+        if (args.length > 0) {
+            port = Integer.parseInt(args[0]);
+        } else {
+            port = 8080;
+        }
+        new Server(port).run();
+    }
+}
