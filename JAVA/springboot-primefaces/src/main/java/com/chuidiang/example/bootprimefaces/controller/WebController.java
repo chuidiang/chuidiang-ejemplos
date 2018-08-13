a7b311e37c68c57a08b1715e8a7add9e27cb08a7
chuidiang <chuidiang@gmail.com>
2018-08-13 17:36:01 +0200

    spring-boot y jsf/primefaces

---
+++ JAVA/springboot-primefaces/src/main/java/com/chuidiang/example/bootprimefaces/controller/WebController.java
@@ -0,0 +1,10 @@
+package com.chuidiang.example.bootprimefaces.controller;
+
+import org.ocpsoft.rewrite.annotation.Join;
+
+
+@Join(path = "/", to = "/index.jsf")
+public class WebController {
+
+}
+
