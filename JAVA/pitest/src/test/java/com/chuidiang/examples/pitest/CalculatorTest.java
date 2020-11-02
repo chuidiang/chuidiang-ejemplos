67a1ab3c36807ebedf2e820b03ddfb64c5c680d7
chuidiang <chuidiang@gmail.com>
2020-11-02 11:58:31 +0100

    pitest: un test no valido

--- JAVA/pitest/src/test/java/com/chuidiang/examples/pitest/CalculatorTest.java
+++ JAVA/pitest/src/test/java/com/chuidiang/examples/pitest/CalculatorTest.java
@@ -15,10 +15,10 @@ public class CalculatorTest {
     public void setUp(){
         calculator = new Calculator();
     }
+
     @Test
     public void simpleTests(){
-        double result = calculator.add(2.0,2.0);
-        Assert.assertEquals("simple add test", 4.0, result ,1e-6);
+        double result;
 
         result=calculator.subtract(10.0,5.0);
         Assert.assertEquals("simple bad subtraction test",5.0,result, 1e-6);
@@ -28,5 +28,14 @@ public class CalculatorTest {
 
         result = calculator.divide(1.0,2.0);
         Assert.assertEquals("simple bad divide test",0.5 ,result, 1e-6);
+    }
+
+    @Test
+    public void noValidTest(){
+        // pitest cambiara dentro del código la suma por una resta, el test seguirá pasando
+        // Este test no es muy valido, hay muchas operaciones que dan cero cuando se les
+        // pasa dos ceros como parametro: suma, resta y multiplicación.
+        double result = calculator.add(0.0,0.0);
+        Assert.assertEquals("simple add test", 0.0, result ,1e-6);
     }
 }
