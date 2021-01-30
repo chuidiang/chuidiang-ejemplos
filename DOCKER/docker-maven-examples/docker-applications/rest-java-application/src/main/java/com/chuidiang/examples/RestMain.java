6e59ae18ddc91176be453846bff5ca2b0c970acc
chuidiang <chuidiang@gmail.com>
2021-01-30 11:45:20 +0100

    docker: RestMain y jar como ejecutable con java -jar

---
+++ DOCKER/docker-maven-examples/docker-applications/rest-java-application/src/main/java/com/chuidiang/examples/RestMain.java
@@ -0,0 +1,21 @@
+package com.chuidiang.examples;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@EnableAutoConfiguration
+public class RestMain {
+
+    // It Opens port 8080
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(RestMain.class, args);
+    }
+}
