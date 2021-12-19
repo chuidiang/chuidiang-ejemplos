5873c4a907ec6a9d8ee18875f451dea875b4dd1a
chuidiang <chuidiang@gmail.com>
2021-12-19 12:21:16 +0100

    Copiado ejemplo de jzy3d

---
+++ JAVA/jzy3d-example/src/main/java/com/chuidiang/examples/Jzy3DExample.java
@@ -0,0 +1,44 @@
+package com.chuidiang.examples;
+
+import org.jzy3d.chart.AWTChart;
+import org.jzy3d.chart.Chart;
+import org.jzy3d.colors.Color;
+import org.jzy3d.colors.ColorMapper;
+import org.jzy3d.colors.colormaps.ColorMapRainbow;
+import org.jzy3d.maths.Range;
+import org.jzy3d.plot3d.builder.Builder;
+import org.jzy3d.plot3d.builder.Mapper;
+import org.jzy3d.plot3d.builder.concrete.OrthonormalGrid;
+import org.jzy3d.plot3d.primitives.Shape;
+import org.jzy3d.plot3d.rendering.canvas.Quality;
+
+/**
+ * @author fjabellan
+ * @date 18/12/2021
+ */
+public class Jzy3DExample {
+    public static void main(String[] args) {
+        // Define a function to plot
+        Mapper mapper = new Mapper() {
+            public double f(double x, double y) {
+                return 10 * Math.sin(x / 10) * Math.cos(y / 20);
+            }
+        };
+
+        // Define range and precision for the function to plot
+        Range range = new Range(-150, 150);
+        int steps = 50;
+
+        // Create a surface drawing that function
+        Shape surface = Builder.buildOrthonormal(new OrthonormalGrid(range, steps), mapper);
+        surface.setColorMapper(new ColorMapper(new ColorMapRainbow(), range));
+        surface.setFaceDisplayed(true);
+        surface.setWireframeDisplayed(false);
+        surface.setWireframeColor(Color.BLACK);
+
+        // Create a chart and add the surface
+        Chart chart = new AWTChart(Quality.Advanced);
+        chart.add(surface);
+        chart.open("Jzy3d Demo", 600, 600);
+    }
+}
