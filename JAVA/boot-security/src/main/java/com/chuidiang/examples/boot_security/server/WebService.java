0e9052491bfb4983ffd86e404159e42e237617f6
chuidiang <chuidiang@gmail.com>
2022-01-08 12:30:05 +0100

    spring boot web service con usuario y password

--- JAVA/boot-security/src/main/java/com/chuidiang/examples/boot_security/server/WebService.java
+++ JAVA/boot-security/src/main/java/com/chuidiang/examples/boot_security/server/WebService.java
@@ -1,19 +1,44 @@
 package com.chuidiang.examples.boot_security.server;
 
-import com.chuidiang.examples.boot_security.client.WebServiceTemplate;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/mi/double/")
-public class WebService extends WebServiceTemplate {
-    @Override
-    public double borrar() {
-        return super.borrar();
+@RequestMapping("mi/double/")
+public class WebService {
+
+    private static final String CAN_BORRAR="hasRole('" +
+            Roles.ROL_ADMIN +
+            "') or hasAuthority('" +
+            Permissions.PERMISSION_BORRAR +
+            "')";
+
+    private static final String CAN_CREAR="hasRole('" +
+            Roles.ROL_ADMIN +
+            "') or hasAuthority('" +
+            Permissions.PERMISSION_CREAR +
+            "')";
+
+    @PreAuthorize(CAN_CREAR)
+    @RequestMapping("crear")
+    public double crear(){
+        System.out.println("Procedo a crear");
+        return Math.random();
     }
 
-    @Override
-    public double crear() {
-        return super.crear();
+    @PreAuthorize(CAN_BORRAR)
+    @RequestMapping("borrar")
+    public double borrar(){
+        System.out.println("Procedo a borrar");
+        return Math.random();
     }
+
+    @PreAuthorize("hasRole('admin')")
+    @RequestMapping("destruyeTodo")
+    public double destruyeTodo(){
+        System.out.println("Me lo cargo todo");
+        return Math.random();
+    }
+
 }
