5784396e7a90fd52f79b8ee49a2d1d91bb64c93c
chuidiang <chuidiang@gmail.com>
2023-01-08 12:17:23 +0100

    pull parser xml python

--- PYTHON/python-xml/pull-parser.py
+++ PYTHON/python-xml/pull-parser.py
@@ -1,9 +1,18 @@
 # Chuidiang 10/12/2022
+# Ejemplo de lectura de un fichero XML con python usando
+# eventos de lectura en vez de leer todo el fichero de golpe.
+
 import xml.etree.ElementTree as ET
 
 if __name__ == '__main__':
-    parser = ET.XMLPullParser()
-    parser.feed("sample.xml")
+    parser = ET.XMLPullParser(['end', 'comment', 'start-ns'])
 
-    iterator = parser.read_events()
-    print(iterator)
+    with open("sample-pull.xml") as data:
+        for line in data:
+            parser.feed(line)
+            iterator = parser.read_events()
+            for event, element in iterator:
+                if 'start-ns' == event:
+                    print(event, element)
+                else:
+                    print(event, element.tag, element.attrib, element.text)
