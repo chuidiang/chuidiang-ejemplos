d37292f32968d877f2555d3ba9cee1abc1e6eb60
chuidiang <chuidiang@gmail.com>
2023-01-21 19:13:37 +0100

    Ejemplo web services python Flask

---
+++ PYTHON/python-rest-flask/readme.md
@@ -0,0 +1,5 @@
+Código de ejemplo del tutorial [Web Services con Python y Flask](https://chuwiki.chuidiang.org/index.php?title=Web_Services_REST_con_Python_y_Flask)
+
+hello-world-flask.py contiene un pequeño Hola Mundo con Flask como ejemplo de aplicación mínima
+
+rest-flask.py contiene un Web Service REST JSON con un ejemplo de GET, POST, PUT y DELETE sobre una lista de usuarios
--- PYTHON/python-rest-flask/rest-flask.py
+++ PYTHON/python-rest-flask/rest-flask.py
@@ -1,48 +1,95 @@
 # Chuidiang 21/01/2023
+# Ejemplo de web service con python Flask
 
-from flask import Flask, abort
+from flask import Flask, abort, request
 import json
 
 app = Flask(__name__)
 
 
-class Data:
+# Datos de usuario
+class UserData:
     def __init__(self, name, age):
         self.name = name
         self.age = age
 
 
+# Lista de usuarios
 users = [
-    Data("Juan", 11),
-    Data("Pedro", 22),
-    Data("Maria", 33)
+    UserData("Juan", 11),
+    UserData("Pedro", 22),
+    UserData("Maria", 33)
 ]
 
 
+# Metodo GET para obtener la lista de todos los usuarios.
 @app.get("/users")
-def get_uses():
+def get_users():
+    encoder = json.JSONEncoder()
     result = []
     for user in users:
         result.append(user.__dict__)
-    return result
+    return encoder.encode(result)
 
 
+# Metodo GET para obtener un usuario concreto por indice
 @app.get("/users/<idx>")
 def get_user(idx):
     try:
         user = users[int(idx)]
-        return user.__dict__
-    except Exception as error:
-        print(error)
+        encoder = json.JSONEncoder()
+        return encoder.encode(user.__dict__)
+    except IndexError:
         abort(404)
+    except ValueError:
+        abort(400)
+    except Exception as error:
+        print(error.__traceback__)
+        abort(500)
 
 
+# Metodo DELETE para borrar un usuario concreto por indice
 @app.delete("/users/<idx>")
 def delete_user(idx):
     try:
         del users[int(idx)]
-        abort(204)
+        return "", 204
+    except (IndexError, ValueError):
+        abort(400)
     except Exception as error:
-        print(error)
+        print(error.__traceback__)
+        abort(500)
+
+
+# Peticion POST para añadir un nuevo usuario
+@app.post("/users")
+def add_user():
+    try:
+        decoder = json.JSONDecoder()
+        encoder = json.JSONEncoder()
+        new_user_dict = decoder.decode(request.data.decode())
+        new_user = UserData(new_user_dict["name"], int(new_user_dict["age"]))
+        users.append(new_user)
+        return encoder.encode(new_user), 201
+    except json.JSONDecodeError:
+        abort(400)
+    except Exception as error:
+        print(error.__class__)
+        abort(500)
+
+
+# Peticion PUT para modificar un usuario por indice.
+@app.put("/users/<idx>")
+def put_user(idx):
+    try:
+        decoder = json.JSONDecoder()
+        new_user_dict = decoder.decode(request.data.decode())
+        new_user = UserData(new_user_dict["name"], int(new_user_dict["age"]))
+        users[int(idx)] = new_user
+        return new_user.__dict__, 200
+    except (IndexError, ValueError, json.JSONDecodeError):
+        abort(400)
+    except Exception as error:
+        print(error.__traceback__)
         abort(500)
 
