3f62cd966be0fe5a7d28eb19c85023d522e857c1
chuidiang <chuidiang@gmail.com>
2023-01-22 12:31:09 +0100

    Ejemplo web services python Flask. Path, params y body content

--- PYTHON/python-rest-flask/hello-world-flask.py
+++ PYTHON/python-rest-flask/hello-world-flask.py
@@ -1,7 +1,7 @@
 # Chuidiang 21/01/2023
 # El Hola Mundo de Flask. Levanta un servidor web y su URL se puede ver "Hello, World!".
 
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -9,3 +9,20 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route("/<path>", methods=['GET', 'POST'])
+def show_path(path):
+
+    content = f"<p>{path}</p>"
+
+    if request.args:
+        content += "<ul>"
+        for key in request.args.keys():
+            content += f"<li>{key} = {request.args[key]}</li>"
+        content += "</ul>"
+
+    if request.data:
+        content += f"<p>data = {request.data}</p>"
+
+    return content
