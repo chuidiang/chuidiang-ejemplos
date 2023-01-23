4bd65afedb5804c4166d752d35cae26cd249cbca
chuidiang <chuidiang@gmail.com>
2023-01-23 20:30:25 +0100

    Cliente REST con modulo requests

--- PYTHON/python-rest-flask/readme.md
+++ PYTHON/python-rest-flask/readme.md
@@ -3,3 +3,6 @@ Código de ejemplo del tutorial [Web Services con Python y Flask](https://chuwik
 hello-world-flask.py contiene un pequeño Hola Mundo con Flask como ejemplo de aplicación mínima
 
 rest-flask.py contiene un Web Service REST JSON con un ejemplo de GET, POST, PUT y DELETE sobre una lista de usuarios
+
+También está aquí el código de un cliente rest-client-request.py que llama al web service levantado por rest-flask.py. 
+El tutorial donde se explica este cliente es en [Ciente Web Service con Python y requests](https://chuwiki.chuidiang.org/index.php?title=Cliente_Web_Service_REST_con_Python_y_requests)
--- PYTHON/python-rest-flask/rest-client-requests.py
+++ PYTHON/python-rest-flask/rest-client-requests.py
@@ -1,8 +1,11 @@
 # Chuidiang 22/01/2023
+# Ejemplo de cliente que llama a web service REST. Para probar este programa
+# debe estar arrancado rest-flask.py
 
 import requests, json
 
 
+# Saca por pantalla el listado de todos los usuarios
 def do_get():
     print("--GET--")
     result = requests.get("http://127.0.0.1:5000/users")
@@ -14,6 +17,7 @@ def do_get():
     print(user)
 
 
+# Saca por pantalla el usuario de índice 1 (Pedro)
 def do_get_idx():
     print("--GET IDX--")
     result = requests.get("http://127.0.0.1:5000/users/1")
@@ -25,6 +29,7 @@ def do_get_idx():
     print(user)
 
 
+# Crea un usuario nuevo (Ana)
 def do_post():
     print("-- POST --")
     new_user = '{"name": "Ana", "age": 44}'
@@ -38,8 +43,9 @@ def do_post():
     do_get()
 
 
+# Modifica la edad del usuario 1 (Pedro)
 def do_put():
-    print("-- POST --")
+    print("-- PUT --")
     new_user = '{"name": "Pedro", "age": 23}'
     result = requests.put("http://127.0.0.1:5000/users/1", data=new_user)
     print (result.status_code)
@@ -51,8 +57,23 @@ def do_put():
     do_get()
 
 
+# Borra el usuario 3 (Ana)
+def do_delete():
+    print("-- DELETE --")
+    result = requests.delete("http://127.0.0.1:5000/users/3")
+    print(result.status_code)
+
+    do_get()
+
+
+# Main que llama a todas las funciones anteriores en orden.
 if __name__ == '__main__':
     do_get()
+    print("---------------")
     do_get_idx()
+    print("---------------")
     do_post()
+    print("---------------")
     do_put()
+    print("---------------")
+    do_delete()
