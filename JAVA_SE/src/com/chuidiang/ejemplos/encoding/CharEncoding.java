7f76ea32d71ad36b6282426dd4c983176eb0e783
chuidiang <chuidiang@gmail.com>
2023-03-05 19:32:22 +0100

    Ejemplos de codificación de caracteres

--- JAVA_SE/src/com/chuidiang/ejemplos/encoding/CharEncoding.java
+++ JAVA_SE/src/com/chuidiang/ejemplos/encoding/CharEncoding.java
@@ -39,31 +39,31 @@ public class CharEncoding {
         // corresponiente a un popurri de caracteres latinos
         // No usa el CharSet defecto (UTF-8 en mi caso) ni otro que se configure.
         int value = 'a';
-        System.out.println(value);
+        System.out.println("a = " + value);
         value = 'ñ';
-        System.out.println(value);
+        System.out.println("ñ = " + value);
     }
 
     private static void inMemorySamples() {
         // Forma de obtener el CharSet por defecto.
         // El CharSet por defecto depende del sistema operativo, habitualmente suele ser UTF-8
-        // Se puede cambiar, al arrancar la máquina virtual, con -Dfile.encoding=UTF16 o el que se deseé
+        // Se puede cambiar, al arrancar la máquina virtual, con -Dfile.encoding=UTF-16 o el que se deseé
         System.out.println("Default CharSet = " + Charset.defaultCharset().name() );
 
         // La máquina virtual java, en mi caso, por defecto es UTF_8
         // Si el texto está pasado a bytes usando la tabla UTF_8
-        byte[] bytes = "ñÑa".getBytes(StandardCharsets.UTF_8);
+        byte[] bytes = "ñÑa".getBytes();
         // Se obtienen unos bytes [-61, -79, -61, -111, 97]
         System.out.println(Arrays.toString(bytes));
         // que la decodificación por defecto interpreta bien.
-        System.out.println("UTF-8 con decodificación por defecto: " + new String(bytes));
+        System.out.println("Con decodificación por defecto: " + new String(bytes));
 
         // pero si se pasan a bytes usando una codificación que  no es la de defecto
         bytes = "ñÑa".getBytes(StandardCharsets.UTF_16);
         // se obtienen unos bytes [-2, -1, 0, -15, 0, -47, 0, 97]
         System.out.println(Arrays.toString(bytes));
         // que la decodificación por defecto no entiende
-        System.out.println("UTF-16 con decodificación por defecto: " + new String(bytes));
+        System.out.println("Decodificación por defecto: " + new String(bytes));
         // U otra decodifación distinta tampoco
         System.out.println("UTF-16 con decodificación ISO-8859-1: " + new String(bytes, StandardCharsets.ISO_8859_1));
         // y hay que decodificar correctamente
---
+++ JAVA_SE/src/com/chuidiang/ejemplos/encoding/readme.md
@@ -0,0 +1,3 @@
+Ejemplo de uso de CharSet / encoding en java.
+
+Está explicado en [Codificacion ASCII Java y CharSet](https://chuidiang.org/index.php?title=Codificacion_ASCII_Java_y_CharSet)
