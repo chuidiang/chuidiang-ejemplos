ba0fd7ce41b786cff68e451628387e87c737a875
chuidiang <chuidiang@gmail.com>
2023-10-22 23:04:40 +0200

    Jasper Report, toqueteando

--- JAVA/jasperreports-examples/src/main/java/com/chuidiang/examples/JasperMainImages.java
+++ JAVA/jasperreports-examples/src/main/java/com/chuidiang/examples/JasperMainImages.java
@@ -2,15 +2,16 @@ package com.chuidiang.examples;
 
 import net.sf.jasperreports.engine.JRException;
 
-import java.sql.SQLException;
+import javax.swing.*;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class JasperMainImages {
-    public static void main(String[] args) throws JRException, SQLException {
+    public static void main(String[] args) throws JRException, IOException {
         Map<String,Object> parameters = new HashMap<>();
         parameters.put("the_code","UA502");
-        parameters.put("from_code_image","src/main/files/the_image.png");
+        parameters.put("from_code_image",new ImageIcon("src/main/files/the_image.png"));
 
         Base.doStuffing("src/main/files/ReportExampleImages.jrxml", parameters);
     }
