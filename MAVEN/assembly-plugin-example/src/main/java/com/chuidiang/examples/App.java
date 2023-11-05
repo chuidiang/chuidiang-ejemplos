8aaa685ed9f131b10cfe712234d7ba25414a4d87
chuidiang <chuidiang@gmail.com>
2023-11-05 15:15:58 +0100

    Ejemplo assembly:single y jar-with-dependencies

---
+++ MAVEN/assembly-plugin-example/src/main/java/com/chuidiang/examples/App.java
@@ -0,0 +1,15 @@
+package com.chuidiang.examples;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Hello world!
+ */
+@Slf4j
+public class App 
+{
+    public static void main( String[] args )
+    {
+        log.info( "Hello World!" );
+    }
+}
