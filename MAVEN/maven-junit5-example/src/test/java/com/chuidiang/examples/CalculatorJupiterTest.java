ac2ea22fa34e6072ad8960d4249729b19a84448a
chuidiang <chuidiang@gmail.com>
2023-11-11 09:08:03 +0100

    Test de Maven con JUnit 5

---
+++ MAVEN/maven-junit5-example/src/test/java/com/chuidiang/examples/CalculatorJupiterTest.java
@@ -0,0 +1,43 @@
+package com.chuidiang.examples;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class CalculatorJupiterTest
+{
+    private Calculator calculator;
+
+    @BeforeEach
+    public void init(){
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void shouldAdd()
+    {
+        Assertions.assertEquals(4, calculator.add(2,2), "Should Add Numbers" );
+    }
+
+    @Test
+    public void shouldSubstract()
+    {
+        Assertions.assertEquals(0, calculator.substract(2,2), "Should Substract Numbers" );
+    }
+
+    @Test
+    public void shouldMultiply()
+    {
+        Assertions.assertEquals(4, calculator.multiply(2,2), "Should Multiply Numbers" );
+    }
+
+    @Test
+    public void shouldDivide()
+    {
+        Assertions.assertEquals(1, calculator.divide(2,2), "Should Divide Numbers" );
+    }
+
+}
