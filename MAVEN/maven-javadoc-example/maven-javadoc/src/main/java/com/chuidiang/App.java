b8dc6f393ea0e01e02da9a334cd5ca1f59de0c87
chuidiang <chuidiang@gmail.com>
2023-11-18 09:36:54 +0100

    doclet

---
+++ MAVEN/maven-javadoc-example/maven-javadoc/src/main/java/com/chuidiang/App.java
@@ -0,0 +1,18 @@
+package com.chuidiang;
+
+/**
+ * Hello world!
+ *
+ * @author Chuidiang
+ */
+public class App 
+{
+    /**
+     * El main
+     * @param args argumentos
+     */
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
