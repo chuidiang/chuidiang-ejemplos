462ca3945cd376a3f0df87341380af72ccb8d20f
chuidiang <chuidiang@gmail.com>
2024-02-11 20:43:57 +0100

    Eemplos Java Stream

--- JAVA_SE/src/com/chuidiang/ejemplos/streams/README.md
+++ JAVA_SE/src/com/chuidiang/ejemplos/streams/README.md
@@ -1,1 +1,12 @@
-Ejemplos de Stream.
+Ejemplos de Stream del tutorial [Java Streams](https://chuidiang.org/index.php?title=Java_Streams)
+
+* java stream filter
+* java stream map
+* java stream findfirst
+* java stream reduce
+* java stream list to map
+* java stream group by
+* java stream foreach
+* java stream anymatch
+* java stream collect
+* java stream flatmap
--- JAVA_SE/src/com/chuidiang/ejemplos/streams/StreamsExample.java
+++ JAVA_SE/src/com/chuidiang/ejemplos/streams/StreamsExample.java
@@ -43,10 +43,10 @@ public class StreamsExample {
             System.out.println(first.getAsInt());
         }
 
-        // Con paralell() hacemos que las operacioens sobre el stream se hagan en paralelo
+        // Con paralell() hacemos que las operaciones sobre el stream se hagan en paralelo
         ints = random.ints(20, 0, 10).parallel();
         // Podemos obtener cualquier elemento del stream con findAny()
-        OptionalInt any = ints.findFirst();
+        OptionalInt any = ints.findAny();
         if (any.isPresent()) {
             System.out.println(first.getAsInt());
         }
@@ -56,10 +56,7 @@ public class StreamsExample {
         // En la primera llamada nos pasará el primer y segundo elemento y nosotros devolvemos un resultado
         // En las siguientes llamadas nos pasará el resultado que dimos en la pasada anterior y el siguiente elemento
         // El siguietne ejemplo suma todos los int
-        OptionalInt reduce = ints.reduce((result, element) -> {
-            System.out.println(result + " " + element);
-            return result + element;
-        });
+        OptionalInt reduce = ints.reduce((result, element) -> result + element);
         if (reduce.isPresent()){
             System.out.println("Reduce = " + reduce.getAsInt());
         }
@@ -82,11 +79,12 @@ public class StreamsExample {
         // collect() permite meter los elementos en un contenedor y nos devuelve el contenedor.
         // Por ejemplo, meter todos los elementos en una Collection, como un Map.
         // Se le pasan tres parametros:
-        // - Algo que le permita crear el contenedor
+        // - Algo que le permite crear el contenedor
         // - Algo que le permita meter elementos en el contenedor
         // - Algo que le permita añadir los elementos de un contenedor a otro. Esto solo tiene sentido si
         // se usan un steram paralelo, ya que cada hilo creará su propio contendor y luego necesitamemos
         // juntarlos todos.
+        final Stream<Data> stream = data.stream();
         Map<Integer, Data> collect = data.parallelStream().collect(
                 () -> new HashMap<>(),
                 (c, e) -> c.put(e.id(), e),
@@ -115,12 +113,25 @@ public class StreamsExample {
 
         // La función map() nos permite transformar un elemento en otro cualquiera. Hay metodos
         // mas especificos como mapToDouble, etc. Obtenemos un Stream con los elementos transformados.
-        data.stream().map(d->d.id()).forEach(System.out::print);
+        ints = IntStream.range(0,10);
+        IntStream doubleValues = ints.map(element -> element * 2);
+        doubleValues.forEach(System.out::print);
         System.out.println();
 
         // Si los elementos son colecciones o tienen varios datos dentro, podemos obteenr con flatMap()
         // un stream en el que cada elemento se convierte en varios.
-        data.stream().flatMap(d->Stream.of(d.id(),d.name())).forEach(System.out::print);
+        final Stream<List<Integer>> listStream = List.of(List.of(1, 2, 3, 4), List.of(5, 6, 7, 8)).parallelStream();
+        final Stream<Integer> integerStream = listStream.flatMap(e -> e.stream());
+        integerStream.forEach(System.out::print);
+        System.out.println();
 
+        // En un stream paralelo trabajan varios hilos
+        ints = random.ints(20, 0, 10).parallel();
+        ints.parallel().forEach(e -> System.out.println(Thread.currentThread().getName() + " "+ e));
+
+
+        final Stream<Integer> stream1 = List.of(1, 2, 3, 4, 5).stream();
+        final Map<String, List<Integer>> collect1 = stream1.collect(Collectors.groupingBy(e -> e % 2==0?"par":"impar"));
+        collect1.forEach((k,v)-> System.out.println(k + " "+ v));
     }
 }
