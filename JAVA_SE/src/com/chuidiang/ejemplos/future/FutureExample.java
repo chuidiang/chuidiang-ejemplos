f95b6a358d75cb4a88af5841ce081aa097daf1a4
chuidiang <chuidiang@gmail.com>
2024-02-23 22:13:01 +0100

    Ejemplos de CompletableFuture

--- JAVA_SE/src/com/chuidiang/ejemplos/future/FutureExample.java
+++ JAVA_SE/src/com/chuidiang/ejemplos/future/FutureExample.java
@@ -1,43 +1,58 @@
 package com.chuidiang.ejemplos.future;
 
-import org.eclipse.jetty.util.Promise;
-
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
-import java.util.concurrent.FutureTask;
+import java.util.concurrent.*;
+import java.util.stream.Stream;
 
 /**
  * @author fjabellan 19/02/2024
+ * Ejemplos con Future y CompletableFuture
  */
 public class FutureExample {
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
-        var future = futureTaskCallable();
-        while(!future.isDone()){
-            Thread.sleep(100);
-        }
-        System.out.println(future.get());
-
-        future = futureTaskRunnable();
-        while(!future.isDone()){
-            Thread.sleep(100);
-        }
-        System.out.println(future.get());
+    /** Hace llamadas a cada uno de los ejemplos */
+    public static void main(String[] args) throws ExecutionException, InterruptedException, TimeoutException {
+//        futureTaskRunnable();
+//        futureTaskCallable();
+//        futureTaskCallableCancel();
+//        futureTaskCallableTimeout();
+//        completableFuture();
+        completableFutureJoin();
+//        completableFutureJoinOfAll();
 
 
+        Thread.sleep(2000);
     }
 
-    private static Future<String> futureTaskCallable() throws ExecutionException, InterruptedException {
+    /**
+     * Crea un FutureTask pasándole un Callable. Los Callable devuelven un valor.
+     * Lanza el FutureTask en un hilo y espera el resultado.
+     *
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
+    private static void futureTaskCallable() throws ExecutionException, InterruptedException {
         FutureTask<String> future = new FutureTask<>(() ->{
             Thread.sleep(1000);
             return "Done!";
         });
 
+//        final ExecutorService executorService = Executors.newFixedThreadPool(1);
+//        executorService.submit(future);
         new Thread(future).start();
 
-        return future;
+        while(!future.isDone()){
+            Thread.sleep(100);
+        }
+        System.out.println("Future Callable" + future.get());
     }
 
-    private static Future<String> futureTaskRunnable() throws ExecutionException, InterruptedException {
+    /**
+     * Crea un FutureTask pasándole un Runnable y el valor que debe devolver cuanto termine
+     * el Runnable.
+     * Lanza el FutureTask en un hilo y espera el resultado.
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
+    private static void futureTaskRunnable() throws ExecutionException, InterruptedException {
         FutureTask<String> future = new FutureTask<>(() ->{
             try {
                 Thread.sleep(1000);
@@ -48,7 +63,139 @@ public class FutureExample {
 
         new Thread(future).start();
 
-        return future;
+        while(!future.isDone()){
+            Thread.sleep(100);
+        }
+        System.out.println("Ruture Runnable "+future.get());
+    }
+
+    /**
+     * Lanza un FutureTask y antes de que termine lo concela.
+     * Vemos que salta una CancellationException si intentamos obtener el valor.
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
+    private static void futureTaskCallableCancel() throws ExecutionException, InterruptedException {
+        FutureTask<String> future = new FutureTask<>(() ->{
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e){
+                System.out.println("FutureTaskCallable Interrumpido");
+                return null;
+            }
+            return "Done!";
+        });
+
+        new Thread(future).start();
+
+        Thread.sleep(100);
+        future.cancel(true);
+
+        try {
+            System.out.println("Future Callable" + future.get());
+        } catch (CancellationException e) {
+            System.out.println("Cancelado "+e.getMessage());
+        }
+    }
+
+    /**
+     * Una CompletableFuture nos permite no quedarnos es espera del resultado.
+     * Con el método thenAccept(), recibiremos el resultado cuando esté disponible.
+     */
+    private static void completableFuture() {
+
+        final CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println("CompletableFuture Interrumpido");
+                return null;
+            }
+            return "Done!";
+        });
+        completableFuture.thenAccept(value -> System.out.println("Terminado " + value));
+
+        System.out.println("No estoy esperando");
+    }
+
+    /**
+     * Lanza un FutureTask y espera por el resultado un tiempo máximo. Si pasa el tiempo y
+     * no hemos obtenido el resultado, se lanza una TimeoutException
+     * @throws ExecutionException
+     */
+    private static void futureTaskCallableTimeout() throws ExecutionException, InterruptedException {
+        FutureTask<String> future = new FutureTask<>(() ->{
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e){
+                System.out.println("FutureTaskCallable Interrumpido");
+                return null;
+            }
+            return "Done!";
+        });
+
+        new Thread(future).start();
+
+        try {
+            System.out.println("Future Callable" + future.get(500, TimeUnit.MILLISECONDS));
+        } catch (TimeoutException e) {
+            System.out.println("Timeout "+e);
+        }
+    }
+
+    private static void completableFutureJoin() {
+
+        final CompletableFuture<String> completableReturn = CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println("CompletableFuture Interrumpido");
+                throw new RuntimeException("Interrumpido");
+            }
+            return "Done!";
+        });
+        final CompletableFuture<String> completableException = CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            throw new RuntimeException("Fallo");
+        });
+//        System.out.println(completableReturn.join());
+//        System.out.println(completableException.join());
+        Stream.of(completableReturn, completableException)
+                .map(future-> future.join())
+                .forEach(value -> System.out.println(value));
+    }
+
+    private static void completableFutureJoinOfAll() {
+
+        final CompletableFuture<String> completableReturn = CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println("CompletableFuture Interrumpido");
+                throw new RuntimeException("Interrumpido");
+            }
+            return "Done!";
+        });
+        final CompletableFuture<String> completableException = CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            throw new RuntimeException("Fallo");
+        });
+        try {
+            CompletableFuture.allOf(completableException, completableReturn).join();
+        } catch (Exception e){
+            System.out.println("allOf falla");
+            return;
+        }
+        System.out.println(completableReturn.join());
+        System.out.println(completableException.join());
     }
 
 }
