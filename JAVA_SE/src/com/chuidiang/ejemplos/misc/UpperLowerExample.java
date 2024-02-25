5705758f1fd553618c60ade75dd1d18bc530e572
chuidiang <chuidiang@gmail.com>
2024-02-25 19:19:18 +0100

    Jugando con letras mayúsculas y minúsculas en java

---
+++ JAVA_SE/src/com/chuidiang/ejemplos/misc/UpperLowerExample.java
@@ -0,0 +1,41 @@
+package com.chuidiang.ejemplos.misc;
+
+/**
+ * @author Chuidiang
+ * date 25/02/2024
+ */
+public class UpperLowerExample {
+    public static void main(String[] args) {
+        // Cadena con mayúsculas, minúsculas, números y caracteres especiales.
+        // \u2164 es cinco en números romanos V
+        String s = "aáA #1çº\u2164";
+
+        System.out.println(s.toLowerCase());
+        System.out.println(s.toUpperCase());
+
+        s.chars().forEach(l -> {
+            System.out.printf("%c is LowerCase %b\n", l, Character.isLowerCase(l));
+        });
+
+        s.chars().forEach(l -> {
+            System.out.printf("%c is UpperCase %b\n", l, Character.isUpperCase(l));
+        });
+
+        s.chars().forEach(l -> {
+            System.out.printf("%c is Digit %b\n", l, Character.isDigit(l));
+        });
+
+        s.chars().forEach(l -> {
+            System.out.printf("%c is Alphabetic %b\n", l, Character.isAlphabetic(l));
+        });
+
+        s.chars().forEach(l -> {
+            System.out.printf("%c is Letter %b\n", l, Character.isLetter(l));
+        });
+
+        s.chars().forEach(l -> {
+            System.out.printf("%c is Letter or digit %b\n", l, Character.isLetterOrDigit(l));
+        });
+
+    }
+}
