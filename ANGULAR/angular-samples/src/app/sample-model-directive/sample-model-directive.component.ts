7eda7f685cf46d1c1dad802fede1b71e1bfd6336
chuidiang <chuidiang@gmail.com>
2024-05-05 20:10:19 +0200

    atributos con angular

--- ANGULAR/angular-samples/src/app/sample-model-directive/sample-model-directive.component.ts
+++ ANGULAR/angular-samples/src/app/sample-model-directive/sample-model-directive.component.ts
@@ -6,16 +6,20 @@ import { Component } from '@angular/core';
   imports: [],
   template: `<div>
     <p>textModel = <span>{{textModel}}</span></p>
-    <p>booleanModel = <span>{{booleanModel}}</span></p>
-    <p>numberModel = <span>{{numberModel}}</span></p>
+    <p>booleanModel = <span [class]='booleanClass'>{{booleanModel}}</span></p>
+    <p>numberModel = <span [class]='numberClass'>{{numberModel}}</span></p>
   </div>
   `,
-  styles: ``
+  styles: `
+    .red {color:red;}
+    .blue {color:blue;}`
 })
 export class SampleModelDirectiveComponent {
   textModel = 'Text';
   booleanModel: boolean = true;
   numberModel: number; 
+  numberClass = 'red';
+  booleanClass = 'blue';
 
   constructor(){
     this.numberModel = 3;
