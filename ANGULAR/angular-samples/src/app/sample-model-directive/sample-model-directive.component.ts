c16bb65b7639d00a615632635e5315dcf94b2468
chuidiang <chuidiang@gmail.com>
2024-05-07 20:06:07 +0200

    angular, ejemplo de event binding

--- ANGULAR/angular-samples/src/app/sample-model-directive/sample-model-directive.component.ts
+++ ANGULAR/angular-samples/src/app/sample-model-directive/sample-model-directive.component.ts
@@ -5,9 +5,10 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   template: `<div>
-    <p>textModel = <span>{{textModel}}</span></p>
+    <p>textModel = <strong>{{textModel}}</strong></p>
     <p>booleanModel = <span [class]='booleanClass'>{{booleanModel}}</span></p>
     <p>numberModel = <span [class]="numberClass">{{numberModel}}</span></p>
+    <p><button (click)="zero()">Zero</button></p>
   </div>
   `,
   styles: `
@@ -27,5 +28,9 @@ export class SampleModelDirectiveComponent {
     setInterval(() =>{
       this.numberModel++;
     },1000);
+  }
+
+  zero(){
+    this.numberModel = 0;
   }
 }
