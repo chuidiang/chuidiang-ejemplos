896d2486fb344d7c2aeb87c115fa3691653d2f5e
chuidiang <chuidiang@gmail.com>
2024-05-12 15:17:12 +0200

    two way binding angular

--- ANGULAR/angular-samples/src/app/sample-model-directive/sample-model-directive.component.ts
+++ ANGULAR/angular-samples/src/app/sample-model-directive/sample-model-directive.component.ts
@@ -1,15 +1,26 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+/* Ejemplos de los cuatro tipos de binding entre el modelo y la vista de angular
+   - String interpolation: Reemplazamos un trozo de html por el contenido de una variable.
+   - Property binding: Reemplazamos el valor de una propiedad de un tag html por el contenido de una variable.
+   - Event binding: Llamamos a un metodo de typescript cuando ocurre en evento en html.
+   - Two Way binding: Enlace en doble sentido, cambios en la variable de typescript se reflejan en el html y 
+                      cambios en el html se reflejan en la variable de typescript.
+*/
 @Component({
   selector: 'app-sample-model-directive',
   standalone: true,
   imports: [FormsModule],
   template: `<div>
+    <!-- con {{variable}} hacemos string interporlation -->
     <p>textModel = <strong>{{textModel}}</strong></p>
+    <!-- con [property]="variable" hacemos property binding -->
     <p>booleanModel = <span [class]='booleanClass'>{{booleanModel}}</span></p>
     <p>numberModel = <span [class]="numberClass">{{numberModel}}</span></p>
+    <!-- con (event)="metodo()" provocamos que se llame a metodo() cuando se produce el evento -->
     <p><button (click)="zero()">Zero</button></p>
+    <!-- con [(ngModel)]="variable" enlazamos el modelo del html (el value del input en este caso) con una variable -->
     <p><input [(ngModel)]="textModel"></p>
   </div>
   `,
