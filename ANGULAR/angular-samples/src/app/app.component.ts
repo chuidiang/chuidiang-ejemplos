fdcb5ab2410191719934fdde8081bfeba08d5ca6
chuidiang <chuidiang@gmail.com>
2024-05-15 20:46:23 +0200

    Ejemplos directivas de atributo Angular

--- ANGULAR/angular-samples/src/app/app.component.ts
+++ ANGULAR/angular-samples/src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { SampleComponentComponent } from './sample-component/sample-component.component';
 import { SampleModelDirectiveComponent } from './sample-model-directive/sample-model-directive.component';
+import { SampleDirectives } from './sample-model-directive/sample-directive.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [SampleComponentComponent, SampleModelDirectiveComponent],
-  template: '<h1>Welcome to {{title}}!</h1>\
-     <app-sample-component></app-sample-component>\
-     <app-sample-model-directive></app-sample-model-directive>',
+  imports: [SampleComponentComponent, SampleModelDirectiveComponent, SampleDirectives],
+  template: `<h1>Welcome to {{title}}!</h1>
+     <h2>Sample Component</h2>
+     <app-sample-component></app-sample-component>
+     <h2>Sample Binding</h2>
+     <app-sample-model-directive></app-sample-model-directive>
+     <h2>Sample Directive</h2>
+     <app-directive-samples></app-directive-samples>`,
   styles: 'h1 {background-color:lightgray}'
 })
 export class AppComponent {
