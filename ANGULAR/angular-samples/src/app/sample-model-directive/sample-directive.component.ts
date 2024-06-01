9d1be5971ecf810de853e888845a387724bfec12
chuidiang <chuidiang@gmail.com>
2024-06-01 19:13:35 +0200

    angular ngFor

--- ANGULAR/angular-samples/src/app/sample-model-directive/sample-directive.component.ts
+++ ANGULAR/angular-samples/src/app/sample-model-directive/sample-directive.component.ts
@@ -6,9 +6,16 @@ import { CommonModule } from "@angular/common";
     standalone: true,
     imports: [CommonModule],
     template: `
+        <p>Directiva de atributo</p>
         <p [ngClass]="classes">A string with classes</p>
         <p [ngClass]="classesMap">A map with classes</p>
         <p [ngClass]="classesArray">An array with classes</p>
+        <p>Directiva estructural</p>
+        <p>Caso 1 simple: <span *ngFor="let aClass of classesArray">{{aClass}} </span></p>
+        <p>Caso 2 index: <span *ngFor="let aClass of classesArray; index as i">{{i}} - {{aClass}} </span></p>
+        <p>Caso 3 template: <ng-template ngFor let-aClass [ngForOf]="classesArray" let-i="index">
+            <span>{{i}} - {{aClass}} </span>
+        </ng-template></p>
     `,
     styles: `
        .big {font-size:x-large;}
@@ -19,4 +26,10 @@ export class SampleDirectives {
     classes = 'big red';
     classesArray = ['red', 'big'];
     classesMap = {'red': true, 'big': true};
+
+    aFunction(index: any, aClass: any){
+        return index+aClass;
+    }
+
+
 }
