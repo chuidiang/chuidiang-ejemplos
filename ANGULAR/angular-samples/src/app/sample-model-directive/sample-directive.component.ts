52a8b28cbfdc6f4881f95e6f9210eba82894229b
chuidiang <chuidiang@gmail.com>
2024-06-02 10:28:23 +0200

    angular ngIf

--- ANGULAR/angular-samples/src/app/sample-model-directive/sample-directive.component.ts
+++ ANGULAR/angular-samples/src/app/sample-model-directive/sample-directive.component.ts
@@ -6,16 +6,19 @@ import { CommonModule } from "@angular/common";
     standalone: true,
     imports: [CommonModule],
     template: `
-        <p>Directiva de atributo</p>
+        <h3>Directiva de atributo ngClass</h3>
         <p [ngClass]="classes">A string with classes</p>
         <p [ngClass]="classesMap">A map with classes</p>
         <p [ngClass]="classesArray">An array with classes</p>
-        <p>Directiva estructural</p>
+        <h3>Directiva estructural ngFor</h3>
         <p>Caso 1 simple: <span *ngFor="let aClass of classesArray">{{aClass}} </span></p>
         <p>Caso 2 index: <span *ngFor="let aClass of classesArray; index as i">{{i}} - {{aClass}} </span></p>
         <p>Caso 3 template: <ng-template ngFor let-aClass [ngForOf]="classesArray" let-i="index">
             <span>{{i}} - {{aClass}} </span>
         </ng-template></p>
+        <h3>Directiva estructural ngIf</h3>
+        <p *ngIf="classes == 'bigred'; else noIf">Se cumple el if</p>
+        <ng-template #noIf><p>No se cumple el if</p></ng-template>
     `,
     styles: `
        .big {font-size:x-large;}
