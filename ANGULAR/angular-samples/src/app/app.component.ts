90f81329bb105e08a46ed2773a6dcaf587e3e59a
chuidiang <chuidiang@gmail.com>
2024-07-28 16:30:47 +0200

    Formulario en Angular

--- ANGULAR/angular-samples/src/app/app.component.ts
+++ ANGULAR/angular-samples/src/app/app.component.ts
@@ -5,12 +5,13 @@ import { SampleDirectives } from './sample-model-directive/sample-directive.comp
 import { SamplePipe } from './sample-pipe/sample-pipe.component';
 import { ServiceComponent } from './sample-service/service.component';
 import { UserComponent } from './sample-httpclient/user.component';
+import { FormComponent } from './sample-form/form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [SampleComponentComponent, SampleModelDirectiveComponent, SampleDirectives,
-    SamplePipe, ServiceComponent, UserComponent],
+    SamplePipe, ServiceComponent, UserComponent, FormComponent],
   template: `<h1>Welcome to {{title}}!</h1>
      <h2>Sample Component</h2>
      <app-sample-component></app-sample-component>
@@ -21,7 +22,8 @@ import { UserComponent } from './sample-httpclient/user.component';
      <h2>Sample Pipe</h2>
      <pipe-sample></pipe-sample>
      <service-component></service-component>
-     <users></users>`,
+     <users></users>
+     <form-component></form-component>`,
   styles: 'h1 {background-color:lightgray}'
 })
 export class AppComponent {
