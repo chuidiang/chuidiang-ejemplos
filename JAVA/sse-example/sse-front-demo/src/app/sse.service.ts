2d5514e4110e30927f08545b412bc06682aac86c
chuidiang <chuidiang@gmail.com>
2024-10-19 14:56:39 +0200

    Angular SSE. README.md y comentado código

--- JAVA/sse-example/sse-front-demo/src/app/sse.service.ts
+++ JAVA/sse-example/sse-front-demo/src/app/sse.service.ts
@@ -4,10 +4,26 @@ import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
+/**
+ * Servicio que está pendiente de la URL que le indiquemos para recibir mensajes
+ * SSE y que notifica al suscriptor que se suscriba.
+ * Crea y devuelve un Observable para permitir la suscripción a un componente externo.
+ */
 export class SseService {
 
+  /**
+   * Necesitamos NgZone para que Angular se entere de que se han tratado los mensajes
+   * SSE y así actualice la vista automáticamente.
+   * @param _zone 
+   */
   constructor(private _zone: NgZone) { }
 
+  /**
+   * Se le pasa la URL por la que llegarán los mensajes SSE y devuelve un Observable
+   * para poder suscribirse a ellos.
+   * @param url 
+   * @returns 
+   */
   getServerSentEvent(url: string): Observable<any> {
     return new Observable(observer => {
       const eventSource = new EventSource(url);
@@ -25,7 +41,8 @@ export class SseService {
         });
       };
 
-      // Al cerrar la conexión, completar el Observable
+      // Cuando se complete el observable porque lo suscriptores se desusicriben,
+      // cerrar EventSource.
       return () => {
         eventSource.close();
       };
