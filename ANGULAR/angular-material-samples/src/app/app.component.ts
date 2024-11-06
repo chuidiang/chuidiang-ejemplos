aae38d529259d980867328a6dc074eeef01c64de
chuidiang <chuidiang@gmail.com>
2024-11-06 19:33:09 +0100

    Ejemplo básico de Angular Material

--- ANGULAR/angular-material-samples/src/app/app.component.ts
+++ ANGULAR/angular-material-samples/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BasicSampleComponent } from './basic-sample/basic-sample.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BasicSampleComponent],
   template: `
     <h1>Welcome to {{title}}!</h1>
-
+    <basic-sample></basic-sample>
     <router-outlet />
   `,
   styles: [],
---
+++ ANGULAR/angular-material-samples/src/app/basic-sample/basic-sample.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'basic-sample',
+  standalone: true,
+  imports: [MatButtonModule, MatIconModule],
+  template: `
+     <button mat-raised-button color="primary">Primary</button>
+     <button mat-flat-button color="accent">Accent</button>
+     <button mat-stroked-button color="warn">Warn</button>
+     <button mat-icon-button>
+        <!-- Listado de todos los posibles iconos en https://fonts.google.com/icons -->
+        <mat-icon>share</mat-icon>
+     </button>
+  `
+})
+export class BasicSampleComponent {}
