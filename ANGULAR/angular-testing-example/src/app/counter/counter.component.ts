307ef9be77b1290423bdcaa6c0f3db209de130ab
chuidiang <chuidiang@gmail.com>
2025-01-26 09:51:49 +0100

    Arreglos en el test de counter angular

--- ANGULAR/angular-testing-example/src/app/counter/counter.component.ts
+++ ANGULAR/angular-testing-example/src/app/counter/counter.component.ts
@@ -2,7 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
-  templateUrl: './counter.component.html',
+  template: `
+  <div>
+    <p id="counter-value">Counter: {{ count }}</p>
+    <button id="increment-button" (click)="increment()">Increment</button>
+    <button id="decrement-button" (click)="decrement()">Decrement</button>
+  </div>
+  `
 })
 export class CounterComponent {
   count = 0;
